3724bc447503a6dca7abed9d5d49e3f7c6e79ed9
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2007-02-12 08:53:41 +0000

    Initial version

---
+++ trunk/sample/sample-ejb/src/main/java/com/example/sample/model/business/SampleBean.java
@@ -0,0 +1,25 @@
+package com.example.sample.model.business;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.example.sample.model.vo.User;
+
+@Stateless
+public class SampleBean implements Sample {
+    @PersistenceContext
+    private EntityManager em;
+    
+    public SampleBean() {
+        // empty
+    }
+
+    public String sayHello(String name) {
+        return "Hello, " + name + "!";
+    }
+
+	public User getUser(Long userId) {
+		return em.find(User.class, userId);
+	}
+}
