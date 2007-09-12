ffb1dc1333dad05873d4622bde499d42ac68aad3
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2007-09-12 11:57:11 +0000

    Updated

---
+++ rfp-analysis/src/etc/setup.bat
@@ -0,0 +1,1 @@
+call mvn install:install-file -DgroupId=OpenForecast -DartifactId=OpenForecast -Dversion=0.4.0 -Dpackaging=jar -Dfile=OpenForecast-0.4.0.jar
--- rfp-analysis/src/main/java/com/fcg/trilogy/ModelAnalyzer.java
+++ rfp-analysis/src/main/java/com/fcg/trilogy/ModelAnalyzer.java
@@ -3,15 +3,14 @@ package com.fcg.trilogy;
 import java.io.FileOutputStream;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFDataFormat;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -20,10 +19,17 @@ import com.fcg.trilogy.model.RFP;
 import com.fcg.trilogy.model.RFPManager;
 
 public class ModelAnalyzer {
-
+	private static String[] match2Project = {
+		"^DOS", "DOS",
+		"^A7", "A7",
+		"^A9000", "A9000",
+		"^AFVP", "AFVP",
+		"^AI", "AI",
+		
+	};
 	public static void analyze(RFPManager manager) throws Exception {
 		Collection<RFP> all = manager.findAll();
-		System.out.println("Total: " + all.size() );
+		System.out.println("Total: " + all.size());
 		SortedSet<Date> dates = new TreeSet<Date>();
 		SortedSet<String> serviceTypes = new TreeSet<String>();
 		for (Iterator<RFP> it = all.iterator(); it.hasNext();) {
@@ -32,62 +38,63 @@ public class ModelAnalyzer {
 			serviceTypes.add(rfp.getServiceType());
 		}
 
-	    HSSFWorkbook wb = new HSSFWorkbook();
-	    FileOutputStream fileOut = new FileOutputStream("rfp-analysis.xls");
-	    HSSFSheet sheet = wb.createSheet("RFP Analysis");
+		HSSFWorkbook wb = new HSSFWorkbook();
+		FileOutputStream fileOut = new FileOutputStream("rfp-analysis.xls");
+		HSSFSheet sheet = wb.createSheet("RFP Analysis");
 
-	    // Date row
-	    HSSFRow row = sheet.createRow((short)0);
-	    HSSFCell cell = row.createCell((short)0);
-	    cell.setCellValue("Date");
-	    short i = 1;
-	    for (Iterator<Date> it = dates.iterator(); it.hasNext();) {
+		// Date row
+		HSSFRow row = sheet.createRow((short) 0);
+		HSSFCell cell = row.createCell((short) 0);
+		cell.setCellValue(new HSSFRichTextString("Date"));
+		short i = 1;
+		for (Iterator<Date> it = dates.iterator(); it.hasNext();) {
 			Date date = it.next();
-		    HSSFCellStyle cellStyle = wb.createCellStyle();
-		    cellStyle.setDataFormat(HSSFDataFormat.getBuiltinFormat("m/d/yy"));
-		    cell = row.createCell(i++);
-		    cell.setCellValue(date);
-		    cell.setCellStyle(cellStyle);
+			HSSFCellStyle cellStyle = wb.createCellStyle();
+			cellStyle.setDataFormat(HSSFDataFormat.getBuiltinFormat("m/d/yy"));
+			cell = row.createCell(i++);
+			cell.setCellValue(date);
+			cell.setCellStyle(cellStyle);
 
 		}
 
-	    short rowIndex = 1;
+		short rowIndex = 1;
 
-	    // Total row
-	    row = sheet.createRow(rowIndex++);
-	    cell = row.createCell((short)0);
-	    cell.setCellValue("Total");
-	    short colIndex = 1;
-	    for (Iterator<Date> it = dates.iterator(); it.hasNext();) {
+		// Total row
+		row = sheet.createRow(rowIndex++);
+		cell = row.createCell((short) 0);
+		cell.setCellValue(new HSSFRichTextString("Total"));
+		short colIndex = 1;
+		for (Iterator<Date> it = dates.iterator(); it.hasNext();) {
 			Date date = it.next();
-		    cell = row.createCell(colIndex++);
-		    cell.setCellValue(countRFPWithDate(all, date));
+			cell = row.createCell(colIndex++);
+			cell.setCellValue(countRFPWithDate(all, date));
 		}
 
-	    // Service types rows
-	    for (Iterator<String> it = serviceTypes.iterator(); it.hasNext();) {
+		// Service types rows
+		for (Iterator<String> it = serviceTypes.iterator(); it.hasNext();) {
 			String serviceType = it.next();
-		    row = sheet.createRow(rowIndex++);
-		    cell = row.createCell((short)0);
-		    cell.setCellValue(serviceType);
-		    colIndex = 1;
-		    for (Iterator<Date> it2 = dates.iterator(); it2.hasNext();) {
+			row = sheet.createRow(rowIndex++);
+			cell = row.createCell((short) 0);
+			cell.setCellValue(new HSSFRichTextString(serviceType));
+			colIndex = 1;
+			for (Iterator<Date> it2 = dates.iterator(); it2.hasNext();) {
 				Date date = it2.next();
-			    cell = row.createCell(colIndex++);
-			    cell.setCellValue(countRFPWithDateAndType(all, date, serviceType));
+				cell = row.createCell(colIndex++);
+				cell.setCellValue(countRFPWithDateAndType(all, date,
+						serviceType));
 			}
 		}
 
-	    wb.write(fileOut);
-	    fileOut.close();
+		wb.write(fileOut);
+		fileOut.close();
 
-	    SortedSet<String> projectNames = new TreeSet<String>();
-	    for (Iterator<RFP> it = all.iterator(); it.hasNext();) {
+		SortedSet<String> projectNames = new TreeSet<String>();
+		for (Iterator<RFP> it = all.iterator(); it.hasNext();) {
 			RFP rfp = it.next();
 			projectNames.add(rfp.getProjectName());
 		}
-	    System.out.println("++++++++++++++++++++++++++++++++++++");
-	    for (Iterator<String> it = projectNames.iterator(); it.hasNext();) {
+		System.out.println("++++++++++++++++++++++++++++++++++++");
+		for (Iterator<String> it = projectNames.iterator(); it.hasNext();) {
 			String name = it.next();
 			System.out.println(name);
 		}
@@ -95,21 +102,23 @@ public class ModelAnalyzer {
 
 	private static int countRFPWithDate(Collection<RFP> all, Date date) {
 		int count = 0;
-		for (Iterator it = all.iterator(); it.hasNext();) {
-			RFP rfp = (RFP) it.next();
+		for (Iterator<RFP> it = all.iterator(); it.hasNext();) {
+			RFP rfp = it.next();
 			if (date.equals(rfp.getDate())) {
-				count ++;
+				count++;
 			}
 		}
 		return count;
 	}
 
-	private static int countRFPWithDateAndType(Collection<RFP> all, Date date, String serviceType) {
+	private static int countRFPWithDateAndType(Collection<RFP> all, Date date,
+			String serviceType) {
 		int count = 0;
-		for (Iterator it = all.iterator(); it.hasNext();) {
+		for (Iterator<RFP> it = all.iterator(); it.hasNext();) {
 			RFP rfp = (RFP) it.next();
-			if (date.equals(rfp.getDate()) && serviceType.equals(rfp.getServiceType())) {
-				count ++;
+			if (date.equals(rfp.getDate())
+					&& serviceType.equals(rfp.getServiceType())) {
+				count++;
 			}
 		}
 		return count;
