45b4c2aa762735c8832d9f51a796280411f40e2d
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2007-09-16 11:52:56 +0000

    Removed discouraged code

--- trunk/MIplugin/org.fcg.tools.mi/src/com/fcg/eclipse/handler/CodeSearchHandler.java
+++ trunk/MIplugin/org.fcg.tools.mi/src/com/fcg/eclipse/handler/CodeSearchHandler.java
@@ -8,10 +8,10 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.browser.IWebBrowser;
 import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.internal.browser.WorkbenchBrowserSupport;
 
 public class CodeSearchHandler extends AbstractHandler {
 
@@ -21,26 +21,24 @@ public class CodeSearchHandler extends AbstractHandler {
 		ITextSelection tsel = null;
 		if (selection instanceof ITextSelection) {
 			tsel = (ITextSelection) selection;
-		} else {
-			return null;
+			String selectedText = tsel.getText();
+			try {
+				URL url = new URL(
+						"http://www.google.com/custom?domains=exampledepot.com&q="
+								+ URLEncoder.encode(selectedText, "UTF-8")
+								+ "&sitesearch=exampledepot.com&client=pub-6001183370374757&forid=1&ie=ISO-8859-1&oe=ISO-8859-1&cof=GALT%3A%23008000%3BGL%3A1%3BDIV%3A%23336699%3BVLC%3A663399%3BAH%3Acenter%3BBGC%3AFFFFFF%3BLBGC%3A336699%3BALC%3A0000FF%3BLC%3A0000FF%3BT%3A000000%3BGFNT%3A0000FF%3BGIMP%3A0000FF%3BFORID%3A1%3B&hl=en");
+				int style = IWorkbenchBrowserSupport.AS_EDITOR
+						| IWorkbenchBrowserSupport.LOCATION_BAR
+						| IWorkbenchBrowserSupport.STATUS;
+				IWorkbenchBrowserSupport wbbs = PlatformUI.getWorkbench()
+						.getBrowserSupport();
+				IWebBrowser browser = wbbs.createBrowser(style, "console",
+						"Console", "Console");
+				browser.openURL(url);
+			} catch (Exception e) {
+				throw new ExecutionException("Cannot open search page. Error:" + e.getMessage(), e);
+			}
 		}
-		String selectedText = tsel.getText();
-
-		try {
-			URL url = new URL("http://www.google.com/custom?domains=exampledepot.com&q="
-				    + URLEncoder.encode(selectedText, "UTF-8")
-				    + "&sitesearch=exampledepot.com&client=pub-6001183370374757&forid=1&ie=ISO-8859-1&oe=ISO-8859-1&cof=GALT%3A%23008000%3BGL%3A1%3BDIV%3A%23336699%3BVLC%3A663399%3BAH%3Acenter%3BBGC%3AFFFFFF%3BLBGC%3A336699%3BALC%3A0000FF%3BLC%3A0000FF%3BT%3A000000%3BGFNT%3A0000FF%3BGIMP%3A0000FF%3BFORID%3A1%3B&hl=en");
-			int style = IWorkbenchBrowserSupport.AS_EDITOR
-					| IWorkbenchBrowserSupport.LOCATION_BAR
-					| IWorkbenchBrowserSupport.STATUS;
-			IWebBrowser browser = WorkbenchBrowserSupport.getInstance()
-					.createBrowser(style, "console", "Console", "Console");
-			browser.openURL(url);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
 		return null;
 	}
-
 }
