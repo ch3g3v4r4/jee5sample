346060cefcd00e21b7dcf80765eb385ca5a1d682
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2007-10-21 21:36:45 +0000

    Removed file/folder

--- ejb3/sample-ejb/src/main/java/com/example/sample/model/business/Sample.java
+++
@@ -1,8 +0,0 @@
-package com.example.sample.model.business;
-
-import com.example.sample.model.entities.Project;
-
-public interface Sample {
-    String sayHello(String name);
-    Project createProject(Project p);
-}
