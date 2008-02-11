e278929a062e1834c58c51ec8accad4d2786a7cc
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2008-02-11 15:43:46 +0000

    initial rev

---
+++ trunk/datastore/src/main/java/org/datastore/Main.java
@@ -0,0 +1,20 @@
+package org.datastore;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.widgets.*;
+
+public class Main {
+public static void main(String[] args){
+Display display = new Display();
+Shell shell = new Shell(display);
+shell.setText("Hello World");
+shell.setSize(200,100);
+shell.open ();
+while (!shell.isDisposed()) {
+if (!display.readAndDispatch())
+display.sleep ();
+}
+display.dispose ();
+}
+}
