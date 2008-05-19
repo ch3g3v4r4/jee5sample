cfc6d5a38fd91dadee34808c21f1a7a2bf734508
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2008-05-19 09:36:23 +0000

    savepoint

--- style3/src/main/java/com/fcg/style3/action/DispatchController.java
+++ style3/src/main/java/com/fcg/style3/action/DispatchController.java
@@ -3,6 +3,7 @@ package com.fcg.style3.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
--- style3/src/main/java/com/fcg/style3/dao/impl/AdminUserDAOImpl.java
+++ style3/src/main/java/com/fcg/style3/dao/impl/AdminUserDAOImpl.java
@@ -1,11 +1,11 @@
 package com.fcg.style3.dao.impl;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fcg.style3.dao.AdminUserDAO;
 import com.fcg.style3.domain.AdminUser;
 
-@Repository
 public class AdminUserDAOImpl extends GenericDAOImpl<AdminUser, Long>
         implements AdminUserDAO {
 
