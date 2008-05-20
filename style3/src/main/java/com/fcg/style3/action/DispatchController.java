c9cbe9a03f64237e177cb02c485903fb06c19319
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2008-05-20 00:07:13 +0000

    add atomikos

--- style3/src/main/java/com/fcg/style3/action/DispatchController.java
+++ style3/src/main/java/com/fcg/style3/action/DispatchController.java
@@ -3,7 +3,6 @@ package com.fcg.style3.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
