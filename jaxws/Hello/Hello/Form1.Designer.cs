683eecd0ef603f2e75303d52c3c3aec94d4885ab
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2009-02-13 06:14:39 +0000

    service client

---
+++ jaxws/Hello/Hello/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Hello
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtString1 = new System.Windows.Forms.TextBox();
+            this.txtString2 = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "String 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "String 2";
+            // 
+            // txtString1
+            // 
+            this.txtString1.Location = new System.Drawing.Point(139, 21);
+            this.txtString1.Name = "txtString1";
+            this.txtString1.Size = new System.Drawing.Size(350, 22);
+            this.txtString1.TabIndex = 2;
+            // 
+            // txtString2
+            // 
+            this.txtString2.Location = new System.Drawing.Point(139, 71);
+            this.txtString2.Name = "txtString2";
+            this.txtString2.Size = new System.Drawing.Size(350, 22);
+            this.txtString2.TabIndex = 3;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(139, 112);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(134, 33);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(139, 165);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(350, 22);
+            this.txtResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 212);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtString2);
+            this.Controls.Add(this.txtString1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtString1;
+        private System.Windows.Forms.TextBox txtString2;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
