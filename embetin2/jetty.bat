04a870764c6f92fcfe7d22c31eadab57cc39a86e
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2009-04-16 15:04:48 +0000

    logging

--- embetin2/jetty.bat
+++ embetin2/jetty.bat
@@ -5,6 +5,6 @@ if "%OS%"=="Windows_NT" @setlocal
 set MAVEN_OPTS=-Xmx512m -XX:MaxPermSize=256m -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000
 
 REM use jta.scope to exclude JTA library from dependency list when running Jetty to avoid classloader errors
-mvn -Dappengine-local-runtime.scope=runtime -Dappengine-api-stubs.scope=runtime jetty:run
+mvn -Dappengine-local-runtime.scope=runtime -Dappengine-api-stubs.scope=runtime -Djava.util.logging.config.file=war/WEB-INF/logging.properties jetty:run
 
 @endlocal
