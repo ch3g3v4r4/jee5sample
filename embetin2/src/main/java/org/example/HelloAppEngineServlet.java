3daf27649a6f2306a0eb0366dc9a64128bd32f2e
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2009-04-17 01:46:09 +0000

    log4j support

--- embetin2/src/main/java/org/example/HelloAppEngineServlet.java
+++ embetin2/src/main/java/org/example/HelloAppEngineServlet.java
@@ -6,17 +6,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.example.manager.EmployeeManager;
 import org.example.model.Employee;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 
+@SuppressWarnings("serial")
 public class HelloAppEngineServlet extends HttpServlet {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(HelloAppEngineServlet.class);
+    private static final Logger LOGGER = Logger.getLogger(HelloAppEngineServlet.class);
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
