ef4506f0fcf8a7ee2567c12974012d357e26275c
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2009-12-07 07:28:50 +0000

    avoid MOJO-1072 bug

--- jaxws/service-war/src/main/java/com/mycompany/webservice/Concatenator.java
+++ jaxws/service-war/src/main/java/com/mycompany/webservice/Concatenator.java
@@ -3,18 +3,11 @@ package com.mycompany.webservice;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-import com.ecocoma.service.domain.whois.Whois;
-import com.ecocoma.service.domain.whois.WhoisService;
-import com.ecocoma.service.domain.whois.WhoisServiceSoap;
-
 @WebService
 public class Concatenator {
 
     @WebMethod
     public String whois(String name) {
-		WhoisService service = new WhoisService();
-		WhoisServiceSoap port = service.getWhoisServiceSoap();
-		Whois result = port.getWhois("DOM-M89714033M", "", name);
-		return result.getDescription().replaceAll("</br>", "\r\n");
+		return "Hello" + name;
     }
 }
