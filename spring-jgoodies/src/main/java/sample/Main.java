a2633365ae6a962f020a0064ab6c9f587c8b3a4c
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2010-10-29 03:28:58 +0000

    redirect jul to slf4j

--- spring-jgoodies/src/main/java/sample/Main.java
+++ spring-jgoodies/src/main/java/sample/Main.java
@@ -1,121 +1,32 @@
 package sample;
 
+import java.util.Date;
+import java.util.logging.LogManager;
+
 import javax.swing.JLabel;
 
 import org.jdesktop.application.SingleFrameApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public class Main extends SingleFrameApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class.getName());
 
-    @Override protected void startup() {
-
+    @Override
+    protected void startup() {
         // Create a label and use properties label.* from Main.properties
         JLabel label = new JLabel();
         label.setName("mylabel");
-
         show(label);
     }
+
     public static void main(String[] args) throws Exception {
+
+        LogManager.getLogManager().reset(); // disable default java.util.logging.ConsoleHandler which logs to stdout
+        SLF4JBridgeHandler.install(); // setup jul-to-slf4j to redirect all java.util.logging events to slf4j
+
+        LOGGER.info("Launching application at " + new Date());
         launch(Main.class, args);
     }
-/*
-    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
-    public static void main(final String[] args) throws Exception {
-
-        // Initialize Look and Feel
-        UIManager.setLookAndFeel("com.jgoodies.looks.plastic.PlasticXPLookAndFeel");
-
-        // Launch the application
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                Main application = new Main();
-                try {
-                    application.doMain(args);
-                } catch (Exception e) {
-                    String message = "Failed to launch " + Main.class;
-                    LOGGER.log(Level.SEVERE, message, e);
-                    throw new Error(message, e);
-                }
-            }
-        });
-
-    }
-
-    private void doMain(String[] args) {
-//      ApplicationContext ctx = new ClassPathXmlApplicationContext("/applicationContext.xml");
-//      ctx.getBean(Main.class).doMain(args);
-
-        // Build Panel
-        JFrame frame = new JFrame("Forms Tutorial :: Quick Start");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.getContentPane().add(buildPanel());
-
-        // Pack, Set Location and Show
-        frame.pack();
-        Dimension paneSize = frame.getSize();
-        Dimension screenSize = frame.getToolkit().getScreenSize();
-        frame.setLocation((screenSize.width - paneSize.width) / 2, (int) ((screenSize.height - paneSize.height) * 0.45));
-        frame.setVisible(true);
-    }
-
-
-    // Building *************************************************************
-    private JTextField companyField;
-    private JTextField contactField;
-    private JTextField ptiField;
-    private JTextField powerField;
-    private JTextField radiusField;
-    private JTextField diameterField;
-
-    public JComponent buildPanel() {
-        // Separating the component initialization and configuration
-        // from the layout code makes both parts easier to read.
-        companyField  = new JTextField();
-        contactField  = new JTextField();
-        ptiField      = new JTextField(6);
-        powerField    = new JTextField(10);
-        radiusField   = new JTextField(8);
-        diameterField = new JTextField(8);
-
-        // Create a FormLayout instance on the given column and row specs.
-        // For almost all forms you specify the columns; sometimes rows are
-        // created dynamically. In this case the labels are right aligned.
-        FormLayout layout = new FormLayout(
-                "right:pref, 3dlu, pref, 7dlu, right:pref, 3dlu, pref", // cols
-                "p, 3dlu, p, 3dlu, p, 9dlu, p, 3dlu, p, 3dlu, p");      // rows
-
-        // Specify that columns 1 & 5 as well as 3 & 7 have equal widths.
-        layout.setColumnGroups(new int[][]{{1, 5}, {3, 7}});
-
-        // Create a builder that assists in adding components to the container.
-        // Wrap the panel with a standardized border.
-        PanelBuilder builder = new PanelBuilder(layout);
-        builder.setDefaultDialogBorder();
-
-        // Obtain a reusable constraints object to place components in the grid.
-        CellConstraints cc = new CellConstraints();
-
-        // Fill the grid with components; the builder offers to create
-        // frequently used components, e.g. separators and labels.
-
-        // Add a titled separator to cell (1, 1) that spans 7 columns.
-        builder.addSeparator("General",   cc.xyw(1,  1, 7));
-        builder.addLabel("Company",       cc.xy (1,  3));
-        builder.add(companyField,         cc.xyw(3,  3, 5));
-        builder.addLabel("Contact",       cc.xy (1,  5));
-        builder.add(contactField,         cc.xyw(3,  5, 5));
-
-        builder.addSeparator("Propeller", cc.xyw(1,  7, 7));
-        builder.addLabel("PTI/kW",        cc.xy (1,  9));
-        builder.add(ptiField,             cc.xy (3,  9));
-        builder.addLabel("Power/kW",      cc.xy (5,  9));
-        builder.add(powerField,           cc.xy (7,  9));
-        builder.addLabel("R/mm",          cc.xy (1, 11));
-        builder.add(radiusField,          cc.xy (3, 11));
-        builder.addLabel("D/mm",          cc.xy (5, 11));
-        builder.add(diameterField,        cc.xy (7, 11));
-
-        // The builder holds the layout container that we now return.
-        return builder.getPanel();
-    }
-*/
 }
