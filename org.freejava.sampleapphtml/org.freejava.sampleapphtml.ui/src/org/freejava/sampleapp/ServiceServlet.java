167b433a398a9f32f9193f870fb914c55198e3fa
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2011-10-15 21:49:51 +0000

    add servlet

---
+++ org.freejava.sampleapphtml/org.freejava.sampleapphtml.ui/src/org/freejava/sampleapp/ServiceServlet.java
@@ -0,0 +1,19 @@
+package org.freejava.sampleapp;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class ServiceServlet extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res)
+			throws ServletException, IOException {
+		res.getWriter().write("HelloServlet");
+
+	}
+
+}
