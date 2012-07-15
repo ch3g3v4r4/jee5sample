f9f49ee566b2145aa9c4b03854887c20bc802678
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2012-07-15 14:18:05 +0000

    cleaned up groovyscripts

--- trunk/groovyscripts/src/main/groovy/ui/SwingUI.groovy
+++ trunk/groovyscripts/src/main/groovy/ui/SwingUI.groovy
@@ -1,22 +1,10 @@
 package ui
-@Grab('com.jgoodies:looks:2.2.2')
-@Grab('com.jgoodies:forms:1.2.1')
-@Grab('com.jgoodies:validation:2.0.1')
-@Grab('com.jgoodies:binding:2.0.6')
-
-
 import groovy.swing.SwingBuilder
 import java.awt.BorderLayout as BL
 import javax.swing.*
-import com.jgoodies.looks.plastic.PlasticLookAndFeel
-import com.jgoodies.looks.plastic.PlasticXPLookAndFeel
-import com.jgoodies.looks.windows.WindowsLookAndFeel
 
 public class SwingUI {
 	public static void main(String[] args) {
-
-		UIManager.setLookAndFeel(new WindowsLookAndFeel())
-
 		def count = 0
 		new SwingBuilder().edt {
 			frame(title:'Frame', size:[300, 300], show: true, defaultCloseOperation: javax.swing.JFrame.EXIT_ON_CLOSE) {
