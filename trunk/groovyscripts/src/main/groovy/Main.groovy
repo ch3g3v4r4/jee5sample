b8db3f7cf47b0b4ebd5378e073104306f5fbe1ea
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2012-07-15 14:49:17 +0000

    added jansi lib for groovyscripts project

--- trunk/groovyscripts/src/main/groovy/Main.groovy
+++ trunk/groovyscripts/src/main/groovy/Main.groovy
@@ -1,5 +1,16 @@
+import org.fusesource.jansi.AnsiConsole;
+
 public class Main {
+
+    public static final String ANSI_CLS = "\u001b[2J";
+    public static final String ANSI_HOME = "\u001b[H";
+    public static final String ANSI_BOLD = "\u001b[1m";
+    public static final String ANSI_AT55 = "\u001b[10;10H";
+    public static final String ANSI_REVERSEON = "\u001b[7m";
+    public static final String ANSI_NORMAL = "\u001b[0m";
+    public static final String ANSI_WHITEONBLUE = "\u001b[37;44m";
+
     public static void main(String[] args) {
-        println 'Hi'
+        AnsiConsole.out.print(ANSI_BOLD + "Press a key..." + ANSI_NORMAL);
     }
 }
