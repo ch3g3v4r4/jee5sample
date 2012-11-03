3d619ad745dbc269450812f59138f2aac8a84b8d
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2012-11-03 11:48:54 +0000

    test ansi color

--- trunk/groovyscripts/src/main/groovy/Main.groovy
+++ trunk/groovyscripts/src/main/groovy/Main.groovy
@@ -1,14 +1,14 @@
 
-import org.fusesource.jansi.AnsiConsole;
-import static org.fusesource.jansi.Ansi.*;
-import static org.fusesource.jansi.Ansi.Color.*;
+import org.fusesource.jansi.AnsiConsole
+import org.fusesource.jansi.Ansi
+import org.fusesource.jansi.Ansi.Color
 
 @Grab(group='commons-lang', module='commons-lang', version='2.6')
 @Grab(group='commons-io', module='commons-io', version='2.4')
 @Grab(group='com.google.guava', module='guava', version='13.0.1')
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		AnsiConsole.systemInstall();
-		System.out.println( ansi().eraseScreen().fg(RED).a("Hello").fg(GREEN).a(" World").reset() );
-    }
+		System.out.println(Ansi.ansi().eraseScreen().fg(Color.RED).a("Hello").fg(Color.GREEN).a(" World").reset());
+	}
 }
