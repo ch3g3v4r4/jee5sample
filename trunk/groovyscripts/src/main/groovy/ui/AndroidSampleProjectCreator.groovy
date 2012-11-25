827b65dcca440755ecd3915b54a6fb73d33ac725
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2012-11-25 10:56:16 +0000

    updated android sdk version

--- trunk/groovyscripts/src/main/groovy/ui/AndroidSampleProjectCreator.groovy
+++ trunk/groovyscripts/src/main/groovy/ui/AndroidSampleProjectCreator.groovy
@@ -3,7 +3,7 @@ package ui
 class AndroidSampleProjectCreator {
 
 	static main(args) {
-		def dir = new File('d:/programs/android_sdk/samples/android-15')
+		def dir = new File('d:/programs/android_sdk_r21/samples/android-17')
 		dir.eachDir {
 			File projectDir = it
 			gen(projectDir)
@@ -13,13 +13,13 @@ class AndroidSampleProjectCreator {
 	static gen(File projectDir) {
 		if (new File(projectDir, "AndroidManifest.xml").exists()) {
 			def projectName = projectDir.name
-			String projectText = AndroidSampleProjectCreator.class.getResourceAsStream("/resources/project").text
+			String projectText = AndroidSampleProjectCreator.class.getResourceAsStream("/project").text
 			new File(projectDir, ".project").text = projectText.replaceAll("\\\$\\{projectName\\}", projectName)
 
-			String classpathText = AndroidSampleProjectCreator.class.getResourceAsStream("/resources/classpath").text
+			String classpathText = AndroidSampleProjectCreator.class.getResourceAsStream("/classpath").text
 			new File(projectDir, ".classpath").text = classpathText.replaceAll("\\\$\\{projectName\\}", projectName)
 
-			String projectPropsText = AndroidSampleProjectCreator.class.getResourceAsStream("/resources/project.properties").text
+			String projectPropsText = AndroidSampleProjectCreator.class.getResourceAsStream("/project.properties").text
 			new File(projectDir, "project.properties").text = projectPropsText.replaceAll("\\\$\\{projectName\\}", projectName)
 		} else {
 			projectDir.eachDir {
