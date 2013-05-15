26929e5a4ff0c3a2640966444b3bb15f9fd6cfd8
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2013-05-15 07:23:34 +0000

    scanner RCP

---
+++ trunk/org.freejava.scanner/README.txt
@@ -0,0 +1,8 @@
+DEVELOPMENT GUIDE
+
+1. Build application from command line
+
+> cd scanner
+> mvn package
+
+The generated zip files will be in scanner\org.freejava.scanner.product\target folder
---
+++ trunk/org.freejava.scanner/org.freejava.scanner.core/src/main/java/org/freejava/scanner/core/Scanner.java
@@ -0,0 +1,9 @@
+package org.freejava.scanner.core;
+
+import java.io.File;
+
+public class Scanner {
+    public static void scan(File folder) {
+        System.out.println("Scanning ...");
+    }
+}
