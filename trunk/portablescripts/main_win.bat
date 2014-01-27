43324790b1228c04b3278f71e01e52e9d930c8be
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2014-01-27 14:20:10 +0000

    Updated for windows

--- trunk/portablescripts/main_win.bat
+++ trunk/portablescripts/main_win.bat
@@ -1,2 +1,8 @@
 @ECHO OFF
 ECHO HELLO
+MKDIR %~dp0downloads
+
+bitsadmin.exe /transfer "Download wget" "http://users.ugent.be/~bpuype/wget/wget.exe" %~dp0downloads\wget.exe
+SET PATH=%PATH%;%~dp0downloads
+
+wget -P "%~dp0downloads" http://download.tuxfamily.org/notepadplus/6.5.3/npp.6.5.3.Installer.exe 
