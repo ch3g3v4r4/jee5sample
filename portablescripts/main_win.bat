12de4e13c74ab21a14dcf89fc592214990d7fc1f
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2014-01-28 14:11:38 +0000

    updated for windows

--- portablescripts/main_win.bat
+++ portablescripts/main_win.bat
@@ -7,4 +7,8 @@ COPY /Y "%TEMP%\wget.exe" "%~dp0downloads\wget.exe"
 SET PATH=%PATH%;%~dp0downloads
 SET DOWNLOADS=%~dp0downloads
 
+rem Notepad++
 wget -nc -P "%DOWNLOADS%" http://download.tuxfamily.org/notepadplus/6.5.3/npp.6.5.3.Installer.exe 
+
+rem Groovy/Grails Tool Suite Downloads based on Eclipse 3.8 http://spring.io/tools/ggts/all
+wget -nc -P "%DOWNLOADS%" http://download.springsource.com/release/STS/3.4.0/dist/e3.8/groovy-grails-tool-suite-3.4.0.RELEASE-e3.8.2-win32-x86_64.zip
