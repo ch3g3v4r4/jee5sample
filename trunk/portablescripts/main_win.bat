c63f4d0ea541658024f27b9de11dc7080d175ac5
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2014-01-28 13:51:28 +0000

    updated for windows

--- trunk/portablescripts/main_win.bat
+++ trunk/portablescripts/main_win.bat
@@ -5,5 +5,6 @@ MKDIR %~dp0downloads
 bitsadmin.exe /transfer "Download wget" "http://users.ugent.be/~bpuype/wget/wget.exe" "%TEMP%\wget.exe"
 COPY /Y "%TEMP%\wget.exe" "%~dp0downloads\wget.exe"
 SET PATH=%PATH%;%~dp0downloads
+SET DOWNLOADS=%~dp0downloads
 
-wget -nc -P "%~dp0downloads" http://download.tuxfamily.org/notepadplus/6.5.3/npp.6.5.3.Installer.exe 
+wget -nc -P "%DOWNLOADS%" http://download.tuxfamily.org/notepadplus/6.5.3/npp.6.5.3.Installer.exe 
