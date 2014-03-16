dfc056251011286f6a96ce951e64eeaa2871821a
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2014-03-16 07:10:54 +0000

    updated to support multiple platforms

--- trunk/tools/hello.bat
+++ trunk/tools/hello.bat
@@ -1,16 +1,9 @@
-:<<"::CMDLITERAL"
-@ECHO OFF
-GOTO :CMDSCRIPT
-::CMDLITERAL
+:; /bin/sh "$(dirname $0)/hello"; exit $?
 
-if [ "$(uname)" == "Darwin" ]; then
-  export MAVEN_OPTS=-XstartOnFirstThread
-fi
-/bin/sh `which mvn` -f "$(dirname $0)/pom.xml" "-Dsource=$(dirname $0)/src/main/groovy/hello.groovy" "-Dscriptdir=$(dirname $0)" groovy:execute
-exit $?
+@echo off
 
-:CMDSCRIPT
-SET SCRIPT_DIR=%~dp0
-REM Remove trailing slash from SCRIPT_DIR
-SET SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
-CALL mvn -f "%~dp0pom.xml" "-Dsource=%~dp0src/main/groovy/hello.groovy" "-Dscriptdir=%SCRIPT_DIR%" groovy:execute
+set SCRIPT_DIR=%~dp0
+rem Remove trailing slash from SCRIPT_DIR
+set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
+
+call mvn -f "%~dp0pom.xml" "-Dsource=%~dp0src/main/groovy/hello.groovy" "-Dscriptdir=%SCRIPT_DIR%" groovy:execute
