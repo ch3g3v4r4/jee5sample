a52e62d9f4269a159b1ce76e84d9923b8a551997
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2014-11-23 05:44:11 +0000

    to eclipse 4.4.1

--- eclipsedropinsbuilder/src/main/java/sample/core/Eclipse.java
+++ eclipsedropinsbuilder/src/main/java/sample/core/Eclipse.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Eclipse {
     private String workDir;
     private String javaDir;
+    private String dictionary;
     private String url;
     private String profile;
     private List<Plugin> plugins;
@@ -15,13 +16,24 @@ public class Eclipse {
     public void setWorkDir(String workDir) {
         this.workDir = workDir;
     }
+    public String getJavaDir() {
+        return javaDir;
+    }
+    public void setJavaDir(String javaDir) {
+        this.javaDir = javaDir;
+    }
+    public String getDictionary() {
+        return dictionary;
+    }
+    public void setDictionary(String dictionary) {
+        this.dictionary = dictionary;
+    }
     public String getUrl() {
         return url;
     }
     public void setUrl(String url) {
         this.url = url;
     }
-
     public String getProfile() {
         return profile;
     }
