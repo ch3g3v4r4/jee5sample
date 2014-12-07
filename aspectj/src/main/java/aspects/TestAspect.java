ea6612afc2d745cb5f44876da9b1623dffff12b7
hathanhthai <hathanhthai@84291c6e-4329-0410-9265-f1fc14926daf>
2014-12-07 21:38:05 +0000

    aspectj sample

---
+++ aspectj/src/main/java/aspects/TestAspect.java
@@ -0,0 +1,14 @@
+package aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class TestAspect {
+
+    @Before("call(* mypackage..*(..))")
+    public void beforeCall() {
+        System.out.println("About to make call to method inside mypackage");
+    }
+
+}
